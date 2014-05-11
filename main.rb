37f3e60da13ef956decb3f0358bdc6cc58473768
Jules Lui <juleslui@yahoo.com>
2014-05-11 11:28:45 +0800

    This is Jules, swear

--- main.rb
+++ main.rb
@@ -1,7 +1,60 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
+
+get '/' do
+  random = Item.all
+  @products = random.sample(10)
+  erb :home
+end
+
+get '/SueUs' do
+  erb :SueUs
+end
+
+get '/products' do
+  @products = Item.all
+  erb :products
+end
+
+get '/payment/:id' do
+  @product = Item.find(params[:id])
+  erb :payment
+end
+
+post '/bought/:id' do
+  @product = Item.find(params[:id])
+  @quantitytobuy = params[:quantity]
+  @mc = MoneyCalculator.new(params[:ones], params[:fives], params[:tens], params[:twenties], params[:fifties], params[:hundreds], params[:five_hundreds], params[:thousands])
+  @money = @mc.return_total
+  @sold = @quantitytobuy.to_i + @product.sold
+  @total = params[:quantity].to_i * @product.price.to_i
+  if @quantitytobuy.to_i > @product.quantity.to_i
+    @errormess = "Insufficient inventory."
+
+  else
+    
+    if @total.to_i > @money.to_i
+      @errormess = "Your money is insufficient!"
+
+    else
+      @change = @mc.change(@total.to_i)
+      @newquant = @product.quantity.to_i - @quantitytobuy.to_i
+      @product.update_attributes!(
+      quantity: @newquant,
+      sold: @sold
+      )
+      erb :bought
+    end
+  end
+end
+
+get '/AboutUs' do
+  erb :AboutUs
+end
+
 get '/admin' do
   @products = Item.all
   erb :admin_index
--- money_calculator.rb
+++ money_calculator.rb
@@ -3,10 +3,52 @@ class MoneyCalculator
   def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
     # each parameter represents the quantity per denomination of money
     # these parameters can be assigned to instance variables and used for computation
+    @ones = ones.to_i*1
+    @fives = fives.to_i*5
+    @tens = tens.to_i*10
+    @twenties = twenties.to_i*20
+    @fifties = fifties.to_i*50
+    @hundreds = hundreds.to_i*100
+    @five_hundreds = five_hundreds.to_i*500
+    @thousands = thousands.to_i*1000
+
+    @total = @ones.to_i + @fives.to_i + @tens.to_i + @twenties.to_i + @fifties.to_i + @hundreds.to_i + @five_hundreds.to_i + @thousands.to_i
+
+    @denom = Hash.new
+    @denom["thou"] = 0  
+    @denom["fivhun"] = 0
+    @denom["hun"] = 0
+    @denom["fif"] = 0
+    @denom["twen"] = 0
+    @denom["ten"] = 0
+    @denom["fiv"] = 0
+    @denom["on"] = 0
 
     # add a method called 'change' that takes in a parameter of how much an item costs
     # and returns a hash of how much change is to be given
     # the hash will use the denominations as keys and the amount per denomination as values
     # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
   end
+
+  def return_total
+    return @total
+  end
+
+  def change(cost)
+
+  		@change = @total - cost.to_i
+      
+      change = @change
+      athousand = change/1000
+      fivehundred = (change%1000)/500
+      onehundred = ((change%1000)%500)/100
+      fifty = (((change%1000)%500)%100)/50
+      twenty = ((((change%1000)%500)%100)%50)/20
+      onezero = (((((change%1000)%500)%100)%50)%20)/10
+      five = ((((((change%1000)%500)%100)%50)%20)%10)/5
+      one = ((((((change%1000)%500)%100)%50)%20)%10)%5
+      return {:on => one, :fiv => five, :ten => onezero, :twen => twenty, :fif => fifty, :hun => onehundred, :fivhun => fivehundred, :thou => athousand}
+  		#insert code that gets the length of @change to be able to get the individual divisions
+  		#such as ex. 1672 = 1000 + 600 + 70 + 2
+    end
 end
